0a048493be7826170dc8dd417153ed7756670af5
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-11-02 11:48:55 -0600

    searchForm working. just need styling and self study questions to meet MVP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
 import Navigation from "./components/navigation";
 import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/SearchForm";
+
 export default function App() {
   return (
     <main>
       <Header />
       <Navigation />
+      <SearchForm />
       <Route exact path="/" component={WelcomePage} />
       <Route path="/characters" component={CharacterList} />
     </main>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 
 export default function SearchForm() {
- 
+  const [search, setSearch] = useState({ name: "" });
+
+  const handleChange = user => {
+    console.log("changed", user.target.name, user.target.value);
+    setSearch({ ...search, [user.target.name]: user.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(search);
+  };
   return (
     <section className="search-form">
-     // Add a search form here
-     <form>
+      <form onSubmit={event => handleSubmit(event)}>
         <input
           placeholder="name"
-          value={name}
+          value={search.name}
           name="name"
+          type="text"
+          onChange={handleChange}
         />
         <button type="submit">Search</button>
       </form>
