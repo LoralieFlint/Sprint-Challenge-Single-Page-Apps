8fe040e611743b5593a8113ae8f857cadb169aea
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-11-02 10:57:40 -0600

    simple search form created in searchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -5,6 +5,14 @@ export default function SearchForm() {
   return (
     <section className="search-form">
      // Add a search form here
+     <form>
+        <input
+          placeholder="name"
+          value={name}
+          name="name"
+        />
+        <button type="submit">Search</button>
+      </form>
     </section>
   );
 }
